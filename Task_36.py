6102b1941e915a00c6e130d4fc32ebaffc4164e2
Polina <pomidorishna@gmail.com>
2023-03-09 22:49:25 +0300

    Задача 36 готова

--- Task_36.py
+++ Task_36.py
@@ -3,3 +3,11 @@
 # Аргументы num_rows и num_columns указывают число строк и столбцов таблицы, которые должны быть распечатаны. 
 # Нумерация строк и столбцов идет с единицы (подумайте, почему не с нуля). 
 # Примечание: бинарной операцией называется любая операция, у которой ровно два аргумента, как, например, у операции умножения.
+
+def print_operation_table(operation, num_rows, num_columns):
+    array = [[operation(i, j) for i in range(1, num_rows + 1)] for j in range(1, num_columns + 1)]
+    for i in array:
+        print(*[f'{x:>3}' for x in i])
+line = int(input("Введите количество строк: "))
+columns = int(input("Введите количество столбцов: "))
+print_operation_table(lambda x, y: x * y, line, columns)
